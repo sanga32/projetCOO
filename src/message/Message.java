e046fbb5574efe9132173425d5c2caf5f98fb159
sanga32 <kev_del@hotmail.fr>
2016-12-03 16:02:03 +0100

    message

--- src/message/Message.java
+++ src/message/Message.java
@@ -1,5 +1,37 @@
 package message;
 
-public abstract class Message {
+import java.sql.Date;
 
+import domaine.Personne;
+
+public abstract class Message {
+	String contenu;
+	Personne expediteur, destinataire;
+	Date dateEnvoi;
+	
+	public Message(String contenu, Personne expediteur, Personne destinataire, Date dateEnvoi) {
+		super();
+		this.contenu = contenu;
+		this.expediteur = expediteur;
+		this.destinataire = destinataire;
+		this.dateEnvoi = dateEnvoi;
+	}
+
+	public String getContenu() {
+		return contenu;
+	}
+
+	public Personne getExpediteur() {
+		return expediteur;
+	}
+
+	public Personne getDestinataire() {
+		return destinataire;
+	}
+
+	public Date getDateEnvoi() {
+		return dateEnvoi;
+	}
+	
+	
 }
