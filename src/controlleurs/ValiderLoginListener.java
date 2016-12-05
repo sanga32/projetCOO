d7114486c7b8a311d1c400ea19cf7e6d0db594ae
sanga32 <kev_del@hotmail.fr>
2016-12-05 22:36:04 +0100

    Debut interface

---
+++ src/controlleurs/ValiderLoginListener.java
@@ -0,0 +1,73 @@
+package controlleurs;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import domaine.Personne;
+import persistance.PersonneMapper;
+import vue.InterfacePersonne;
+
+/**
+ * Listener du bouton permettant de valider le login et d'accéder à la page d'information
+ * @author Kevin
+ *
+ */
+
+public class ValiderLoginListener implements ActionListener{
+
+	private JTextField saisieID;
+	private JPanel j;
+
+	public ValiderLoginListener(JTextField id, JPanel f) {
+		// TODO Auto-generated constructor stub
+		saisieID = id;
+		j = f;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		Personne p;
+		if ( saisieID.getText().equals("")){
+			JOptionPane.showMessageDialog(null, "Veuillez entrer un ID", "Message d'erreur",  JOptionPane.ERROR_MESSAGE);
+
+
+		}else{
+
+			try {
+				
+				int id = Integer.parseInt(saisieID.getText());
+				p = PersonneMapper.getInstance().findById(id);
+
+
+				try {
+					
+					InterfacePersonne ip = null;
+					try {
+						ip = new InterfacePersonne(p);
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					j.removeAll();
+					j.add(ip);
+					j.updateUI();
+				} catch (NullPointerException e) {
+					// TODO Auto-generated catch block
+					JOptionPane.showMessageDialog(null, "Aucune personne avec cet ID", "Message d'erreur",  JOptionPane.ERROR_MESSAGE);
+
+				}
+			} catch (NumberFormatException e ){
+				JOptionPane.showMessageDialog(null, "Veuillez entrer un ID correct !", "Message d'erreur",  JOptionPane.ERROR_MESSAGE);
+
+			}
+		}
+
+	}
+
+}
---
+++ src/vue/Login.java
@@ -0,0 +1,57 @@
+package vue;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import controller.ValiderLoginListener;
+
+/**
+ * Classe Login permet la connexion d'un utilisateur 
+ * @author Kevin
+ *
+ */
+
+@SuppressWarnings("serial")
+public class Login extends JPanel{
+
+	public Login(){
+
+		super();
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = gbc.gridy = 0; // la grille commence en (0, 0)
+		gbc.gridheight = 1; // valeur par défaut - peut s'étendre sur une seule ligne.
+		gbc.gridwidth = 1; // valeur par défaut - peut s'étendre sur une seule ligne.
+		gbc.anchor = GridBagConstraints.PAGE_START; // ou BASELINE_LEADING mais pas WEST.
+		gbc.insets = new Insets(10, 15, 0, 0);
+		gbc.fill = GridBagConstraints.NONE;	
+		
+		
+		JLabel jl = new JLabel("Votre id: ");
+		JTextField id = new JTextField();
+		JButton valider = new JButton("OK");
+		
+		jl.setPreferredSize(new Dimension(50, 35));
+		id.setPreferredSize(new Dimension(80, 35));
+		id.addActionListener(new ValiderLoginListener(id, this));
+		valider.setPreferredSize(new Dimension(80, 35));
+		valider.addActionListener(new ValiderLoginListener(id, this));
+		this.add(jl, gbc);
+		
+		gbc.gridx = 1;
+		this.add(id, gbc);
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.gridwidth = 2;
+		this.add(valider, gbc);
+		
+
+	}
+}
