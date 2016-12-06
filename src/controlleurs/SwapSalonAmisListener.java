e832ae28ca88667aeb6d483a4a1e002257363df9
sanga32 <kev_del@hotmail.fr>
2016-12-06 21:10:45 +0100

    bouton swap terminé

--- src/controlleurs/SwapSalonAmisListener.java
+++ src/controlleurs/SwapSalonAmisListener.java
@@ -3,12 +3,32 @@ package controlleurs;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JPanel;
+
+import vue.West;
+
 public class SwapSalonAmisListener implements ActionListener{
+
+	West west ;
+	
+	
+	
+	public SwapSalonAmisListener(West west) {
+		super();
+		this.west = west;
+	}
+
+
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		if(e.getActionCommand().equals("Salons")){
+			west.getJListAmis();
+		} else {
+			west.getJListSalons();
+		}
+		west.updateUI();
 	}
 
 }
--- src/vue/West.java
+++ src/vue/West.java
@@ -1,6 +1,7 @@
 package vue;
 
 import java.awt.Dimension;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
@@ -10,23 +11,29 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 import controlleurs.JListAmisController;
+import controlleurs.SwapSalonAmisListener;
 import domaine.Personne;
+import domaine.Salon;
+import persistance.SalonMapper;
 
 public class West extends JPanel {
 
 	Personne p;
+	JButton swap;
 	
 	public West(Personne p, InterfaceChat interfaceChat) {
 		// TODO Auto-generated constructor stub
 		BoxLayout boxLayout = new BoxLayout(this, BoxLayout.Y_AXIS); // top to bottom
 		this.setLayout(boxLayout);
 		this.p = p;
-		JButton swap = new JButton("Salons");
-		this.add(swap);
-		this.add(getJListAmis());
+		
+		getJListSalons();
+		
+		getJListAmis();
 	}
 	
-	public JScrollPane getJListAmis(){
+	public void getJListAmis(){
+		this.removeAll();
 		JList<Personne> jl = new JList<Personne>();
 		DefaultListModel<Personne> lmodel = new DefaultListModel<Personne>();
 
@@ -37,10 +44,36 @@ public class West extends JPanel {
 		jl.setModel(lmodel);
 		jl.getSelectionModel().addListSelectionListener(new JListAmisController());
 
-		JScrollPane listScrollPane = new JScrollPane(jl, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		JScrollPane listScrollPane = new JScrollPane(jl, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 		listScrollPane.setPreferredSize(new Dimension(115, 150));
-		
-		return listScrollPane;
+		swap = new JButton("Amis");
+		swap.addActionListener(new SwapSalonAmisListener(this));
+		this.add(swap);
+		this.add(listScrollPane);
+
 	}
+	
+	public void getJListSalons(){
+		this.removeAll();
+		JList<Salon> jl = new JList<Salon>();
+		DefaultListModel<Salon> lmodel = new DefaultListModel<Salon>();
+
+		SalonMapper.getInstance().setSalons(p);
+		
+		for ( Salon s : p.getSalons()){
+			lmodel.addElement(s);
+		}
+
+		jl.setModel(lmodel);
+		jl.getSelectionModel().addListSelectionListener(new JListAmisController());
+
+		JScrollPane listScrollPane = new JScrollPane(jl, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		listScrollPane.setPreferredSize(new Dimension(115, 150));
+		swap = new JButton("Salons");
+		swap.addActionListener(new SwapSalonAmisListener(this));
+		this.add(swap);
+		this.add(listScrollPane);
+	}
+	
 
 }
