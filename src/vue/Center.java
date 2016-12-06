5322689e875f8193fd914ed7f13e166dba770a26
sanga32 <kev_del@hotmail.fr>
2016-12-06 16:00:26 +0100

    Modification profil en cours

--- src/vue/Center.java
+++ src/vue/Center.java
@@ -6,7 +6,7 @@ import domaine.Personne;
 
 public class Center extends JPanel {
 
-	public Center(Personne p) {
+	public Center(Personne p, InterfaceChat interfaceChat) {
 		// TODO Auto-generated constructor stub
 	}
 
