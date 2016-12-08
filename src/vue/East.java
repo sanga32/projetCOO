bff2b030c3ce701495be4795805781a77318177b
sanga32 <kev_del@hotmail.fr>
2016-12-08 22:02:56 +0100

    east

--- src/vue/East.java
+++ src/vue/East.java
@@ -1,11 +1,18 @@
 package vue;
 
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
+import controlleurs.CreerSalonListener;
 import controlleurs.JListAmisController;
+import controlleurs.SwapSalonAmisListener;
 import domaine.Personne;
 import domaine.Salon;
 import persistance.PersonneMapper;
@@ -19,9 +26,10 @@ public class East extends JPanel {
 	public East(InterfaceChat interfaceChat) {
 		// TODO Auto-generated constructor stub
 		BoxLayout boxLayout = new BoxLayout(this, BoxLayout.Y_AXIS); // top to
-																		// bottom
+		// bottom
 		this.setLayout(boxLayout);
-
+		this.setPreferredSize(new Dimension(120, 300));
+		this.setBackground(Color.white);
 	}
 
 	public void getPersonnePrive(String personne) {
@@ -46,6 +54,21 @@ public class East extends JPanel {
 
 		jl.setModel(lmodel);
 		this.add(jl);
+	}
+
+	public void init(){
+		this.removeAll();
+		JList<Salon> jl = new JList<Salon>();
+		DefaultListModel<Salon> lmodel = new DefaultListModel<Salon>();
+
+
+		jl.setModel(lmodel);
+
+		JScrollPane listScrollPane = new JScrollPane(jl, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		listScrollPane.setPreferredSize(new Dimension(115, 150));
+
+
+		this.add(listScrollPane);
 	}
 
 }
