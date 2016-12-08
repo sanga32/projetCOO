d2181a3e1a9c15228fa99d3ef78ff9ff5af80e53
Alex <Alex@Alex-PC>
2016-12-08 21:54:49 +0100

    personne privé

--- src/vue/East.java
+++ src/vue/East.java
@@ -8,21 +8,31 @@ import javax.swing.JPanel;
 import controlleurs.JListAmisController;
 import domaine.Personne;
 import domaine.Salon;
+import persistance.PersonneMapper;
 import persistance.SalonMapper;
 
 public class East extends JPanel {
-	
+
 	Salon s;
 	Personne p;
 
 	public East(InterfaceChat interfaceChat) {
 		// TODO Auto-generated constructor stub
-		BoxLayout boxLayout = new BoxLayout(this, BoxLayout.Y_AXIS); // top to bottom
+		BoxLayout boxLayout = new BoxLayout(this, BoxLayout.Y_AXIS); // top to
+																		// bottom
 		this.setLayout(boxLayout);
-	
+
 	}
 
 	public void getPersonnePrive(String personne) {
+		this.removeAll();
+		JList<Personne> jl = new JList<Personne>();
+		DefaultListModel<Personne> lmodel = new DefaultListModel<Personne>();
+		Personne p = PersonneMapper.getInstance().findByLogin(personne);
+		lmodel.addElement(p);
+
+		jl.setModel(lmodel);
+		this.add(jl);
 	}
 
 	public void getJListPersonneSalons(String salon) {
@@ -30,10 +40,10 @@ public class East extends JPanel {
 		JList<Personne> jl = new JList<Personne>();
 		DefaultListModel<Personne> lmodel = new DefaultListModel<Personne>();
 		Salon s = SalonMapper.getInstance().findByNom(salon);
-		for ( Personne pers : s.getPersonnes()){
+		for (Personne pers : s.getPersonnes()) {
 			lmodel.addElement(pers);
 		}
-		
+
 		jl.setModel(lmodel);
 		this.add(jl);
 	}
