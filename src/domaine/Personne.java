c7e3fdb21df90e0e6416ff7f0244e52331361634
sanga32 <kev_del@hotmail.fr>
2016-12-20 17:31:40 +0100

    maj javadoc

--- src/domaine/Personne.java
+++ src/domaine/Personne.java
@@ -23,14 +23,14 @@ public abstract class Personne {
 	List<SousInteret> sousInterets;
 	List<Salon> salons;
 	List<Notification> notifications;
-	
+
 	public Personne(){
 		this.amis = new VirtualProxyListAmi(id);
 		this.interets = new ArrayList<Interet>();
 		this.sousInterets = new ArrayList<SousInteret>();
 		this.salons = new ArrayList<Salon>();
 	}
-	
+
 	public Personne(int id, String login, String mdp, String nom, String prenom) {
 		super();
 		this.id = id;
@@ -44,7 +44,7 @@ public abstract class Personne {
 		this.salons = new ArrayList<Salon>();
 		this.notifications = new ArrayList<Notification>();
 	}
-	
+
 	public Personne(int id, String login, String mdp, String nom, String prenom, List<Interet>  interets, List<SousInteret> sousInterets) {
 		super();
 		this.id = id;
@@ -58,42 +58,73 @@ public abstract class Personne {
 		this.salons = new ArrayList<Salon>();
 		this.notifications = new ArrayList<Notification>();
 	}
-	
+
 	public abstract boolean isAdmin() throws SQLException;
-	
+
+	/**
+	 * return true si la personne est modérateur du salon passé en paramètre
+	 * @param s
+	 * @return
+	 * @throws SQLException
+	 */
 	public abstract boolean isModo(Salon s) throws SQLException;
-	
+
 	public void addPersonne(Personne p){
 		this.amis.add(p);
 	}
-	
+
 	public void removePersonne(Personne p){
 		this.amis.remove(p);
 	}
-	
+
+	/**
+	 * Ajoute l'intérêt passé en paramètre de la liste d'intérets
+	 * @param i
+	 */
 	public void addInteret(Interet i){
 		this.interets.add(i);
 	}
-	
+
+	/**
+	 * Supprime l'intérêt passé en paramètre de la liste d'intérêts
+	 * @param i
+	 */
 	public void removeInteret(Interet i){
 		this.interets.remove(i);
 	}
+	
+	/**
+	 * Ajoute le sous intérêt passé en paramètre à la liste de sous intérêts
+	 * @param si
+	 */
 	public void addSousInteret(SousInteret si){
 		this.sousInterets.add(si);
 	}
-	
+
+	/** 
+	 * supprime le sous intérêts passé en paramètre de la liste de sous intérêts
+	 * @param si
+	 */
 	public void removeSousInteret(SousInteret si){
 		this.sousInterets.remove(si);
 	}
+	/**
+	 * Ajoute le salon passé en paramètre a la liste des salons
+	 * @param s
+	 */
 	public void addSalon(Salon s){
 		this.salons.add(s);
 	}
-	
+
+	/**
+	 * Supprime le salon passé en paramètre de la liste de salons 
+	 * @param s
+	 */
 	public void removeSalon(Salon s){
 		this.salons.remove(s);
 	}
-	
-	
+
+
 	public int getId() {
 		return id;
 	}
@@ -127,7 +158,7 @@ public abstract class Personne {
 	public List<Personne> getAmis() {
 		return ((VirtualProxyListAmi) amis).getAmis();
 	}
-	
+
 	public void setAmis(List<Personne> amis) {
 		this.amis = amis;
 	}
@@ -149,19 +180,27 @@ public abstract class Personne {
 	public void setSalons(List<Salon> salons) {
 		this.salons = salons;
 	}
-	
+
 	public String toString(){
 		return login ;
 	}
 
+	/**
+	 * supprime la personne passée en paramètre de la liste d'amis
+	 * @param destinataire
+	 */
 	public void deleteAmi(Personne destinataire) {
 		// TODO Auto-generated method stub
 		amis.remove(destinataire);
 	}
-	
+
+	/**
+	 * Ajoute la personne passée en paramètre dans la liste d'ami
+	 * @param p
+	 */
 	public void addAmi(Personne p){
 		amis.add(p);
 	}
-	
-	
+
+
 }
