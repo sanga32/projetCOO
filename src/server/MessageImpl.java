db77a71a807175de28b7b8e199c75c968af9b495
sanga32 <kev_del@hotmail.fr>
2017-03-19 15:27:16 +0100

    Discussion qui correspond au salon du côté serveur

---
+++ src/server/MessageImpl.java
@@ -0,0 +1,16 @@
+package server;
+
+import Interface.MessageInterface;
+import message.Message;
+
+public class MessageImpl implements MessageInterface{
+
+	Message m ;
+	
+	@Override
+	public Message getMessage() {
+		// TODO Auto-generated method stub
+		return m;
+	}
+
+}
